398b741230f19d03828d0fcf3baaa13fc5761acb
Nghia Bui <nghia.bngtn@gmail.com>
2025-04-06 23:17:48 +0700

    no change

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,9 @@
   networking.hostName = "NixOS"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # enable hardware sensors
+  hardware.sensor.iio.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -138,6 +141,7 @@
     packages = with pkgs; [
       kdePackages.kate
       caprine
+      obsidian
     ];
   };
 
@@ -165,7 +169,8 @@
     fastfetch
     docker
     devpod-desktop
-    wl-clipboard 
+    wl-clipboard
+    lm_sensors
   ];
 
   # Automatic Update
